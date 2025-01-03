998c3a608e4189bdaf48108632f290cbeae46519
maw619 <marcowolff619@gmail.com>
2025-01-03 14:19:46 -0500

    added the categories

--- dash/forms.py
+++ dash/forms.py
@@ -6,13 +6,14 @@ from dash.models import PasswordEntry
 class AddForm(ModelForm):
     class Meta:
         model = PasswordEntry
-        fields = ['site_name', 'site_url', 'username', 'encrypted_password','remarks']
+        fields = ['site_name', 'site_url', 'username', 'encrypted_password','remarks', 'category']
         labels = { 
             'site_name': 'NAME',
             'site_url': 'URL',
             'username': 'USERNAME',
             'encrypted_password': 'PASSWORD',
             'remarks': 'REMARKS',
+            'category':'CATEGORY'
         }
         widgets = {  
             'site_name': forms.TextInput(attrs={'class': "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}),
@@ -20,6 +21,7 @@ class AddForm(ModelForm):
             'username': forms.TextInput(attrs={'class': "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}),
             'encrypted_password': forms.TextInput(attrs={'class': "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}),
             'remarks': forms.Textarea(attrs={'class':  "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500", 'rows': 3}),  
+            'category': forms.Select()
         }
 
 
--- dash/models.py
+++ dash/models.py
@@ -7,6 +7,12 @@ class User(AbstractUser):
 
 
 class PasswordEntry(models.Model):
+    mycat = (
+        ('youtube','YOUTUBE'),
+        ('gmail','GMAIL'),
+        ('other','OTHER'),
+    )
+    category = models.CharField(choices=mycat, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="password_entries")
     site_name = models.CharField(max_length=255)
     site_url = models.URLField()
