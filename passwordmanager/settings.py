8f457bcae226879e77dadbe0ebff871d677e08a4
maw619 <marcowolff619@gmail.com>
2025-01-05 23:34:44 -0500

    sdf

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY='django-insecure-#8+c11g2j(jk11av20v6r*1jql$p(tpv_@zcrwcv_rspx9dk4o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
