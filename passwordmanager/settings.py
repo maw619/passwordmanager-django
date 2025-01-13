3fc75e0ca4f4b049049a2cf6a2b0b09844e4e08e
maw619 <marcowolff619@gmail.com>
2025-01-12 21:52:25 -0500

    added heroicons to index.html

--- passwordmanager/settings.py
+++ passwordmanager/settings.py
@@ -121,23 +121,23 @@ WSGI_APPLICATION = 'passwordmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-#         'NAME': 'mydb', 
-#         'USER': 'postgres', 
-#         'PASSWORD': 'password', 
-#         'HOST': 'db', 
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'mydb', 
+        'USER': 'postgres', 
+        'PASSWORD': 'passdb', 
+        'HOST': 'localhost', 
+        'PORT': '5432'
+    }
+}
 
 # DATABASES ={}
 # DATABASES['default'] = dj_database_url.parse(
